fdc0207fb3142c5df0e6b3793fe6f98d1e7a8377
Joshua <hola@joshua.cl>
2024-03-03 12:32:09 -0300

    Declaraciones de variables VAR vs LET

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
+/* =================================================================
 
+ Sección 2: Cómo funcionan las declaraciones y las uniones (binding)?
 
+================================================================= */
 
+/* -----------------------------------------------------------------
+                Declaraciones de variables VAR vs LET
+----------------------------------------------------------------- */
 
+// - El ambito de una variable (llamado "scope" en ingles) es la zona del programa en la que se define la variable. Javascript define dos ambitos para las variables: Global y Funcion
 
+// - Cuando usamos la declaracion "var" y hacemos referencia a esta variable antes de su definicion, javascript interpretara como "undefined" su valor, cuando debiese tirar un error de declaracion, ya que tecnicamente queremos usar una variable antes de ser definida, esto se resuelve con la declaracion "let"
 
+// console.log( mensaje )
 
+// var mensaje = "Hola mundo"
 
+// - En el siguiente ejemplo al declarar la variable con "let" y al hacer referencia antes de su definicion Javascript nos tirara un error de referencia explicando que no podemos acceder a la variable antes de ser inicializada, lo cual es una manera mas clara de indentificar el problema
+
+console.log( mensaje )
+
+let mensaje = "Hola mundo"
